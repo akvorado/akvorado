dd369ad31ae81f8aa266255382446141cd7a7319
Vincent Bernat <vincent@bernat.ch>
2022-05-31 19:29:40 +0200

    console/frontend: on 401, redirect to a fake login page
    We cannot really put a login page.

--- console/frontend/src/router.js
+++ console/frontend/src/router.js
@@ -4,15 +4,31 @@ import VisualizePage from "@/views/VisualizePage.vue";
 import DocumentationPage from "@/views/DocumentationPage.vue";
 import ErrorPage from "@/views/ErrorPage.vue";
 
+const checkAuthenticated = async (to, from, next) => {
+  const response = await fetch("/api/v0/console/user/info");
+  if (response.status == 401) {
+    next({ name: "401", query: { redirect: to.path } });
+  } else {
+    next();
+  }
+};
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", name: "Home", component: HomePage, meta: { title: "Home" } },
+    {
+      path: "/",
+      name: "Home",
+      component: HomePage,
+      meta: { title: "Home" },
+      beforeEnter: [checkAuthenticated],
+    },
     {
       path: "/visualize",
       name: "Visualize",
       component: VisualizePage,
       meta: { title: "Visualize" },
+      beforeEnter: [checkAuthenticated],
     },
     {
       path: "/visualize/:state",
@@ -20,14 +36,20 @@ const router = createRouter({
       component: VisualizePage,
       meta: { title: "Visualize" },
       props: (route) => ({ routeState: route.params.state }),
+      beforeEnter: [checkAuthenticated],
     },
-    { path: "/docs", redirect: "/docs/intro" },
+    {
+      path: "/docs",
+      redirect: "/docs/intro",
+      beforeEnter: [checkAuthenticated],
+    },
     {
       path: "/docs/:id",
       name: "Documentation",
       component: DocumentationPage,
       meta: { title: "Documentation" },
       props: true,
+      beforeEnter: [checkAuthenticated],
     },
     {
       path: "/:pathMatch(.*)",
@@ -35,6 +57,13 @@ const router = createRouter({
       component: ErrorPage,
       meta: { title: "Not found" },
       props: { error: "Not found!" },
+    },
+    {
+      path: "/login",
+      name: "401",
+      component: ErrorPage,
+      meta: { title: "Not authorized" },
+      props: { error: "Not authorized!" },
     },
   ],
 });
