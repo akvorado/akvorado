5c79f8e74e26254639688309e647337eb8402032
Vincent Bernat <vincent@bernat.ch>
2022-06-01 12:33:45 +0200

    console/frontend: give up on fixed position for summary bar to truncate

--- console/frontend/src/views/VisualizePage/RequestSummary.vue
+++ console/frontend/src/views/VisualizePage/RequestSummary.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed z-10 hidden h-7 w-full gap-3 whitespace-nowrap border-b border-gray-300 bg-gray-100 px-4 pt-1 pb-2 text-xs text-gray-400 dark:border-slate-700 dark:bg-slate-800 dark:text-gray-500 lg:flex"
+    class="z-10 hidden h-7 w-full items-center gap-3 whitespace-nowrap border-b border-gray-300 bg-gray-100 px-4 text-xs text-gray-400 dark:border-slate-700 dark:bg-slate-800 dark:text-gray-500 lg:flex"
   >
     <span v-if="request.graphType" class="shrink-0">
       <ChartPieIcon class="inline h-4 px-1 align-middle" />
@@ -9,6 +9,20 @@
     <span v-if="request.start && request.end" class="shrink-0">
       <CalendarIcon class="inline h-4 px-1 align-middle" />
       <span class="align-middle">{{ start }} — {{ end }}</span>
+    </span>
+    <span
+      v-if="request.dimensions && request.dimensions.length > 0"
+      class="truncate"
+      :title="request.dimensions.join(', ')"
+    >
+      <AdjustmentsIcon class="inline h-4 px-1 align-middle" />
+      <span class="truncate align-middle">{{
+        request.dimensions.join(", ")
+      }}</span>
+    </span>
+    <span v-if="request.filter" class="truncate" :title="request.filter">
+      <FilterIcon class="inline h-4 px-1 align-middle" />
+      <span class="max-w-xs align-middle">{{ request.filter }}</span>
     </span>
     <span v-if="request.limit" class="shrink-0">
       <ArrowUpIcon class="inline h-4 px-1 align-middle" />
@@ -20,16 +34,7 @@
         { bps: "ᵇ⁄ₛ", pps: "ᵖ⁄ₛ" }[request.units] || requests.units
       }}</span>
     </span>
-    <span v-if="request.dimensions && request.dimensions.length > 0">
-      <AdjustmentsIcon class="inline h-4 px-1 align-middle" />
-      <span class="align-middle">{{ request.dimensions.join(", ") }}</span>
-    </span>
-    <span v-if="request.filter" :title="request.filter">
-      <FilterIcon class="inline h-4 px-1 align-middle" />
-      <span class="max-w-xs truncate align-middle">{{ request.filter }}</span>
-    </span>
   </div>
-  <div class="hidden h-7 lg:block"></div>
 </template>
 
 <script setup>
