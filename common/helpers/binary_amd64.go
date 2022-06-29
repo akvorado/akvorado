ac037c9a56579cbd65b51a4faa9aed5582eefb09
Vincent Bernat <vincent@bernat.ch>
2022-06-29 13:58:20 +0200

    common/helpers: use "unsafe" to get endianness

--- common/helpers/binary_amd64.go
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Free Mobile
-// SPDX-License-Identifier: AGPL-3.0-only
-
-package helpers
-
-import "encoding/binary"
-
-// NativeEndian implements binary native byte order.
-var NativeEndian = binary.LittleEndian
