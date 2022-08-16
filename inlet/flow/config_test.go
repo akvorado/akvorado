akvorado/akvorado

commit 3f1dc38052342ee730143eb7a8ff1b82ce8c4e9e

Revert "inlet/snmp: do not deprecate default-community"
This reverts commit e5d3d3fbc3060338b5ed43fbcdc4f5a8faabeac4. When
adding SNMPv3, it will be odd to have this. Just put the extra mile to
deprecate it correctly.

inlet/flow/config.go
@@ -67,19 +67,14 @@ func ConfigurationUnmarshallerHook() mapstructure.DecodeHookFunc {
 		for _, key := range mapKeys {
 			var keyStr string
 			// YAML may unmarshal keys to interfaces
-			if key.Kind() == reflect.String {
+			if helpers.ElemOrIdentity(key).Kind() == reflect.String {
 				keyStr = key.String()
-			} else if key.Kind() == reflect.Interface && key.Elem().Kind() == reflect.String {
-				keyStr = key.Elem().String()
 			} else {
 				continue
 			}
 			switch strings.ToLower(keyStr) {
 			case "type":
-				inputTypeVal := from.MapIndex(key)
-				if inputTypeVal.Kind() == reflect.Interface {
-					inputTypeVal = inputTypeVal.Elem()
-				}
+				inputTypeVal := helpers.ElemOrIdentity(from.MapIndex(key))
 				if inputTypeVal.Kind() != reflect.String {
 					return nil, fmt.Errorf("type should be a string not %s", inputTypeVal.Kind())
 				}

inlet/snmp/config.go
@@ -4,9 +4,12 @@
 package snmp
 
 import (
+	"reflect"
 	"time"
 
 	"akvorado/common/helpers"
+
+	"github.com/mitchellh/mapstructure"
 )
 
 // Configuration describes the configuration for the SNMP client
@@ -19,10 +22,6 @@ type Configuration struct {
 	CacheCheckInterval time.Duration `validate:"ltefield=CacheRefresh"`
 	// CachePersist defines a file to store cache and survive restarts
 	CachePersistFile string
-	// DefaultCommunity is the default SNMP community to use
-	DefaultCommunity string `validate:"required"`
-	// Communities is a mapping from exporter IPs to communities
-	Communities *helpers.SubnetMap[string]
 	// PollerRetries tell how many time a poller should retry before giving up
 	PollerRetries int `validate:"min=0"`
 	// PollerTimeout tell how much time a poller should wait for an answer
@@ -31,16 +30,25 @@ type Configuration struct {
 	PollerCoalesce int `validate:"min=0"`
 	// Workers define the number of workers used to poll SNMP
 	Workers int `validate:"min=1"`
+
+	// Communities is a mapping from exporter IPs to communities
+	Communities *helpers.SubnetMap[string]
 }
 
 // DefaultConfiguration represents the default configuration for the SNMP client.
 func DefaultConfiguration() Configuration {
+	communities, err := helpers.NewSubnetMap(map[string]string{
+		"::/0": "public",
+	})
+	if err != nil {
+		panic(err)
+	}
 	return Configuration{
 		CacheDuration:      30 * time.Minute,
 		CacheRefresh:       time.Hour,
 		CacheCheckInterval: 2 * time.Minute,
 		CachePersistFile:   "",
-		DefaultCommunity:   "public",
+		Communities:        communities,
 		PollerRetries:      1,
 		PollerTimeout:      time.Second,
 		PollerCoalesce:     10,
@@ -48,6 +56,53 @@ func DefaultConfiguration() Configuration {
 	}
 }
 
+// ConfigurationUnmarshallerHook normalize SNMP configuration:
+//   - append default-community to communities (as ::/0)
+//   - ensure we have a default value for communities
+func ConfigurationUnmarshallerHook() mapstructure.DecodeHookFunc {
+	return func(from, to reflect.Value) (interface{}, error) {
+		if from.Kind() != reflect.Map || from.IsNil() || from.Type().Key().Kind() != reflect.String || to.Type() != reflect.TypeOf(Configuration{}) {
+			return from.Interface(), nil
+		}
+
+		// default-community → communities
+		var defaultKey, mapKey *reflect.Value
+		fromMap := from.MapKeys()
+		for i, k := range fromMap {
+			if helpers.MapStructureMatchName(k.String(), "DefaultCommunity") {
+				defaultKey = &fromMap[i]
+			} else if helpers.MapStructureMatchName(k.String(), "Communities") {
+				mapKey = &fromMap[i]
+			}
+		}
+		var communities reflect.Value
+		if mapKey != nil {
+			communities = helpers.ElemOrIdentity(from.MapIndex(*mapKey))
+		}
+		if defaultKey != nil && !helpers.ElemOrIdentity(from.MapIndex(*defaultKey)).IsZero() {
+			if mapKey == nil {
+				// Use the fact we can set the default value directly.
+				from.SetMapIndex(reflect.ValueOf("communities"), from.MapIndex(*defaultKey))
+			} else {
+				communities.SetMapIndex(reflect.ValueOf("::/0"), from.MapIndex(*defaultKey))
+			}
+		} else {
+			// default-community should contain ::/0
+			if mapKey == nil {
+				from.SetMapIndex(reflect.ValueOf("communities"), reflect.ValueOf("public"))
+			} else if !communities.MapIndex(reflect.ValueOf("::/0")).IsValid() {
+				communities.SetMapIndex(reflect.ValueOf("::/0"), reflect.ValueOf("public"))
+			}
+		}
+		if defaultKey != nil {
+			from.SetMapIndex(*defaultKey, reflect.Value{})
+		}
+
+		return from.Interface(), nil
+	}
+}
+
 func init() {
+	helpers.AddMapstructureUnmarshallerHook(ConfigurationUnmarshallerHook())
 	helpers.AddMapstructureUnmarshallerHook(helpers.SubnetMapUnmarshallerHook[string]())
 }

inlet/snmp/config_test.go
@@ -5,12 +5,122 @@ package snmp
 
 import (
 	"testing"
+	"time"
 
 	"akvorado/common/helpers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mitchellh/mapstructure"
 )
 
 func TestDefaultConfiguration(t *testing.T) {
 	if err := helpers.Validate.Struct(DefaultConfiguration()); err != nil {
 		t.Fatalf("validate.Struct() error:\n%+v", err)
+	}
+}
+
+func TestConfigurationUnmarshallerHook(t *testing.T) {
+	cases := []struct {
+		Description string
+		Input       gin.H
+		Output      Configuration
+	}{
+		{
+			Description: "nil",
+			Input:       nil,
+		}, {
+			Description: "empty",
+			Input:       gin.H{},
+			Output: Configuration{
+				Communities: helpers.MustNewSubnetMap(map[string]string{
+					"::/0": "public",
+				}),
+			},
+		}, {
+			Description: "no communities, no default community",
+			Input: gin.H{
+				"cache-refresh":  "10s",
+				"poller-retries": 10,
+			},
+			Output: Configuration{
+				CacheRefresh:  10 * time.Second,
+				PollerRetries: 10,
+				Communities: helpers.MustNewSubnetMap(map[string]string{
+					"::/0": "public",
+				}),
+			},
+		}, {
+			Description: "communities, no default community",
+			Input: gin.H{
+				"communities": gin.H{
+					"203.0.113.0/25":   "public",
+					"203.0.113.128/25": "private",
+				},
+			},
+			Output: Configuration{
+				Communities: helpers.MustNewSubnetMap(map[string]string{
+					"::/0":                     "public",
+					"::ffff:203.0.113.0/121":   "public",
+					"::ffff:203.0.113.128/121": "private",
+				}),
+			},
+		}, {
+			Description: "no communities, default community",
+			Input: gin.H{
+				"default-community": "private",
+			},
+			Output: Configuration{
+				Communities: helpers.MustNewSubnetMap(map[string]string{
+					"::/0": "private",
+				}),
+			},
+		}, {
+			Description: "communities, default community",
+			Input: gin.H{
+				"default-community": "private",
+				"communities": gin.H{
+					"203.0.113.0/25":   "public",
+					"203.0.113.128/25": "private",
+				},
+			},
+			Output: Configuration{
+				Communities: helpers.MustNewSubnetMap(map[string]string{
+					"::/0":                     "private",
+					"::ffff:203.0.113.0/121":   "public",
+					"::ffff:203.0.113.128/121": "private",
+				}),
+			},
+		}, {
+			Description: "communities, default-community empty",
+			Input: gin.H{
+				"default-community": "",
+				"communities": gin.H{
+					"203.0.113.0/25":   "public",
+					"203.0.113.128/25": "private",
+				},
+			},
+			Output: Configuration{
+				Communities: helpers.MustNewSubnetMap(map[string]string{
+					"::/0":                     "public",
+					"::ffff:203.0.113.0/121":   "public",
+					"::ffff:203.0.113.128/121": "private",
+				}),
+			},
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.Description, func(t *testing.T) {
+			var got Configuration
+			decoder, err := mapstructure.NewDecoder(helpers.GetMapStructureDecoderConfig(&got))
+			if err != nil {
+				t.Fatalf("NewDecoder() error:\n%+v", err)
+			}
+			err = decoder.Decode(tc.Input)
+			if err != nil {
+				t.Fatalf("Decode() error:\n%+v", err)
+			} else if diff := helpers.Diff(got, tc.Output); diff != "" {
+				t.Fatalf("Decode() (-got, +want):\n%s", diff)
+			}
+		})
 	}
 }

orchestrator/clickhouse/config.go
@@ -90,12 +90,8 @@ type NetworkAttributes struct {
 // compatibility.
 func NetworkAttributesUnmarshallerHook() mapstructure.DecodeHookFunc {
 	return func(from, to reflect.Value) (interface{}, error) {
-		if to.Kind() == reflect.Interface {
-			to = to.Elem()
-		}
-		if from.Kind() == reflect.Interface {
-			from = from.Elem()
-		}
+		from = helpers.ElemOrIdentity(from)
+		to = helpers.ElemOrIdentity(to)
 		if to.Type() != reflect.TypeOf(NetworkAttributes{}) {
 			return from.Interface(), nil
 		}

common/helpers/reflect.go
@@ -1,0 +1,15 @@
+// SPDX-FileCopyrightText: 2022 Free Mobile
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package helpers
+
+import "reflect"
+
+// ElemOrIdentity returns Elem() of the provided value if this is an
+// interface, else it returns the value unmodified.
+func ElemOrIdentity(value reflect.Value) reflect.Value {
+	if value.Kind() == reflect.Interface {
+		return value.Elem()
+	}
+	return value
+}

common/helpers/subnetmap.go
@@ -24,7 +24,7 @@ type SubnetMap[V any] struct {
 // Lookup will search for the most specific subnet matching the
 // provided IP address and return the value associated with it.
 func (sm *SubnetMap[V]) Lookup(ip net.IP) (V, bool) {
-	if sm == nil || sm.tree == nil {
+	if sm.tree == nil {
 		var value V
 		return value, false
 	}
@@ -91,11 +91,8 @@ func SubnetMapUnmarshallerHook[V any]() mapstructure.DecodeHookFunc {
 			// First case, we have a map
 			iter := from.MapRange()
 			for i := 0; iter.Next(); i++ {
-				k := iter.Key()
+				k := ElemOrIdentity(iter.Key())
 				v := iter.Value()
-				if k.Kind() == reflect.Interface {
-					k = k.Elem()
-				}
 				if k.Kind() != reflect.String {
 					return nil, fmt.Errorf("key %d is not a string (%s)", i, k.Kind())
 				}

inlet/snmp/root.go
@@ -285,7 +285,7 @@ func (c *Component) dispatchIncomingRequest(request lookupRequest) {
 func (c *Component) pollerIncomingRequest(request lookupRequest) {
 	community, ok := c.config.Communities.Lookup(net.ParseIP(request.ExporterIP))
 	if !ok {
-		community = c.config.DefaultCommunity
+		community = "public"
 	}
 
 	// Avoid querying too much exporters with errors

inlet/snmp/root_test.go
@@ -41,8 +41,8 @@ func TestLookup(t *testing.T) {
 func TestSNMPCommunities(t *testing.T) {
 	r := reporter.NewMock(t)
 	configuration := DefaultConfiguration()
-	configuration.DefaultCommunity = "notpublic"
-	configuration.Communities = helpers.MustNewSubnetMap(map[string]string{
+	configuration.Communities, _ = helpers.NewSubnetMap(map[string]string{
+		"::/0":                 "notpublic",
 		"::ffff:127.0.0.1/128": "public",
 		"::ffff:127.0.0.2/128": "private",
 	})

commit 334ed0f56f4e6985a247e512721945edb81db1c4

inlet/flow: fix input configuration parsing
Commit 3f1dc3805234 introduced a regression. On an interface value,
`.String()` is not equal to `.Elem().String()`.

inlet/flow/config.go
@@ -69,7 +69,7 @@ func ConfigurationUnmarshallerHook() mapstructure.DecodeHookFunc {
 			var keyStr string
 			// YAML may unmarshal keys to interfaces
 			if helpers.ElemOrIdentity(key).Kind() == reflect.String {
-				keyStr = key.String()
+				keyStr = helpers.ElemOrIdentity(key).String()
 			} else {
 				continue
 			}

inlet/flow/config_test.go
@@ -4,6 +4,7 @@
 package flow
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -19,27 +20,29 @@ import (
 func TestDecodeConfiguration(t *testing.T) {
 	cases := []struct {
 		Name     string
-		From     interface{}
-		Source   interface{}
+		From     func() interface{}
+		Source   func() interface{}
 		Expected interface{}
 	}{
 		{
 			Name: "from empty configuration",
-			From: Configuration{},
-			Source: gin.H{
-				"inputs": []gin.H{
-					{
-						"type":    "udp",
-						"decoder": "netflow",
-						"listen":  "192.0.2.1:2055",
-						"workers": 3,
-					}, {
-						"type":    "udp",
-						"decoder": "sflow",
-						"listen":  "192.0.2.1:6343",
-						"workers": 3,
+			From: func() interface{} { return Configuration{} },
+			Source: func() interface{} {
+				return gin.H{
+					"inputs": []gin.H{
+						{
+							"type":    "udp",
+							"decoder": "netflow",
+							"listen":  "192.0.2.1:2055",
+							"workers": 3,
+						}, {
+							"type":    "udp",
+							"decoder": "sflow",
+							"listen":  "192.0.2.1:6343",
+							"workers": 3,
+						},
 					},
-				},
+				}
 			},
 			Expected: Configuration{
 				Inputs: []InputConfiguration{{
@@ -60,29 +63,33 @@ func TestDecodeConfiguration(t *testing.T) {
 			},
 		}, {
 			Name: "from existing configuration",
-			From: Configuration{
-				Inputs: []InputConfiguration{{
-					Decoder: "netflow",
-					Config:  udp.DefaultConfiguration(),
-				}, {
-					Decoder: "sflow",
-					Config:  udp.DefaultConfiguration(),
-				}},
-			},
-			Source: gin.H{
-				"inputs": []gin.H{
-					{
-						"type":    "udp",
-						"decoder": "netflow",
-						"listen":  "192.0.2.1:2055",
-						"workers": 3,
+			From: func() interface{} {
+				return Configuration{
+					Inputs: []InputConfiguration{{
+						Decoder: "netflow",
+						Config:  udp.DefaultConfiguration(),
 					}, {
-						"type":    "udp",
-						"decoder": "sflow",
-						"listen":  "192.0.2.1:6343",
-						"workers": 3,
+						Decoder: "sflow",
+						Config:  udp.DefaultConfiguration(),
+					}},
+				}
+			},
+			Source: func() interface{} {
+				return gin.H{
+					"inputs": []gin.H{
+						{
+							"type":    "udp",
+							"decoder": "netflow",
+							"listen":  "192.0.2.1:2055",
+							"workers": 3,
+						}, {
+							"type":    "udp",
+							"decoder": "sflow",
+							"listen":  "192.0.2.1:6343",
+							"workers": 3,
+						},
 					},
-				},
+				}
 			},
 			Expected: Configuration{
 				Inputs: []InputConfiguration{{
@@ -103,19 +110,23 @@ func TestDecodeConfiguration(t *testing.T) {
 			},
 		}, {
 			Name: "change type",
-			From: Configuration{
-				Inputs: []InputConfiguration{{
-					Decoder: "netflow",
-					Config:  udp.DefaultConfiguration(),
-				}},
+			From: func() interface{} {
+				return Configuration{
+					Inputs: []InputConfiguration{{
+						Decoder: "netflow",
+						Config:  udp.DefaultConfiguration(),
+					}},
+				}
 			},
-			Source: gin.H{
-				"inputs": []gin.H{
-					{
-						"type":  "file",
-						"paths": []string{"file1", "file2"},
+			Source: func() interface{} {
+				return gin.H{
+					"inputs": []gin.H{
+						{
+							"type":  "file",
+							"paths": []string{"file1", "file2"},
+						},
 					},
-				},
+				}
 			},
 			Expected: Configuration{
 				Inputs: []InputConfiguration{{
@@ -127,22 +138,26 @@ func TestDecodeConfiguration(t *testing.T) {
 			},
 		}, {
 			Name: "only set one item",
-			From: Configuration{
-				Inputs: []InputConfiguration{{
-					Decoder: "netflow",
-					Config: &udp.Configuration{
-						Workers:   2,
-						QueueSize: 100,
-						Listen:    "127.0.0.1:2055",
-					},
-				}},
+			From: func() interface{} {
+				return Configuration{
+					Inputs: []InputConfiguration{{
+						Decoder: "netflow",
+						Config: &udp.Configuration{
+							Workers:   2,
+							QueueSize: 100,
+							Listen:    "127.0.0.1:2055",
+						},
+					}},
+				}
 			},
-			Source: gin.H{
-				"inputs": []gin.H{
-					{
-						"listen": "192.0.2.1:2055",
+			Source: func() interface{} {
+				return gin.H{
+					"inputs": []gin.H{
+						{
+							"listen": "192.0.2.1:2055",
+						},
 					},
-				},
+				}
 			},
 			Expected: Configuration{
 				Inputs: []InputConfiguration{{
@@ -157,33 +172,48 @@ func TestDecodeConfiguration(t *testing.T) {
 		},
 	}
 	for _, tc := range cases {
-		t.Run(tc.Name, func(t *testing.T) {
-			got := tc.From
+		for _, viaYAML := range []bool{false, true} {
+			t.Run(fmt.Sprintf("%s (from YAML: %v)", tc.Name, viaYAML), func(t *testing.T) {
+				var source interface{}
+				if viaYAML {
+					// Encode and decode with YAML
+					out, err := yaml.Marshal(tc.Source())
+					if err != nil {
+						t.Fatalf("yaml.Marshal() error:\n%+v", err)
+					}
+					if err := yaml.Unmarshal(out, &source); err != nil {
+						t.Fatalf("yaml.Unmarshal() error:\n%+v", err)
+					}
+				} else {
+					// Just use as is
+					source = tc.Source()
+				}
+				got := tc.From()
 
-			decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-				Result:           &got,
-				ErrorUnused:      true,
-				Metadata:         nil,
-				WeaklyTypedInput: true,
-				MatchName: func(mapKey, fieldName string) bool {
-					key := strings.ToLower(strings.ReplaceAll(mapKey, "-", ""))
-					field := strings.ToLower(fieldName)
-					return key == field
-				},
-				DecodeHook: ConfigurationUnmarshallerHook(),
+				decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+					Result:           &got,
+					ErrorUnused:      true,
+					Metadata:         nil,
+					WeaklyTypedInput: true,
+					MatchName: func(mapKey, fieldName string) bool {
+						key := strings.ToLower(strings.ReplaceAll(mapKey, "-", ""))
+						field := strings.ToLower(fieldName)
+						return key == field
+					},
+					DecodeHook: ConfigurationUnmarshallerHook(),
+				})
+				if err != nil {
+					t.Fatalf("NewDecoder() error:\n%+v", err)
+				}
+				if err := decoder.Decode(source); err != nil {
+					t.Fatalf("Decode() error:\n%+v", err)
+				}
+
+				if diff := helpers.Diff(got, tc.Expected); diff != "" {
+					t.Fatalf("Decode() (-got, +want):\n%s", diff)
+				}
 			})
-			if err != nil {
-				t.Fatalf("NewDecoder() error:\n%+v", err)
-			}
-			if err := decoder.Decode(tc.Source); err != nil {
-				t.Fatalf("Decode() error:\n%+v", err)
-			}
-
-			expected := tc.Expected
-			if diff := helpers.Diff(got, expected); diff != "" {
-				t.Fatalf("Decode() (-got, +want):\n%s", diff)
-			}
-		})
+		}
 	}
 }
 