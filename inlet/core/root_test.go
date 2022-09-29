d31cb933fce2b711e829bbf2d1314e5108edfbd8
Vincent Bernat <vincent@bernat.ch>
2022-09-29 23:34:52 +0200

    inlet/core: /api/v0/inlet/flows endpoint can return protobuf
    We use content negotiation to return the right format.

--- inlet/core/root_test.go
+++ inlet/core/root_test.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	netHTTP "net/http"
 	"testing"
@@ -24,6 +25,7 @@ import (
 	"akvorado/common/reporter"
 	"akvorado/inlet/bmp"
 	"akvorado/inlet/flow"
+	"akvorado/inlet/flow/decoder"
 	"akvorado/inlet/geoip"
 	"akvorado/inlet/kafka"
 	"akvorado/inlet/snmp"
@@ -326,4 +328,40 @@ func TestCore(t *testing.T) {
 			t.Fatalf("GET /api/v0/inlet/flows got less than 4 flows (%d)", count)
 		}
 	})
+
+	// Test HTTP flow clients using Protobuf
+	t.Run("http flows", func(t *testing.T) {
+		c.httpFlowFlushDelay = 20 * time.Millisecond
+
+		client := netHTTP.Client{}
+		req, err := netHTTP.NewRequest("GET", fmt.Sprintf("http://%s/api/v0/inlet/flows?limit=1", c.d.HTTP.LocalAddr()), nil)
+		if err != nil {
+			t.Fatalf("GET /api/v0/inlet/flows:\n%+v", err)
+		}
+		req.Header.Set("Accept", "application/x-protobuf")
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("GET /api/v0/inlet/flows:\n%+v", err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			t.Fatalf("GET /api/v0/inlet/flows status code %d", resp.StatusCode)
+		}
+
+		// Produce one flow
+		kafkaProducer.ExpectInputAndSucceed()
+		flowComponent.Inject(t, flowMessage("192.0.2.142", 434, 677))
+
+		// Decode it
+		raw, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatalf("ReadAll() error:\n%+v", err)
+		}
+		var flow decoder.FlowMessage
+		buf := proto.NewBuffer(raw)
+		if err := buf.DecodeMessage(&flow); err != nil {
+			t.Fatalf("DecodeMessage() error:\n%+v", err)
+		}
+	})
+
 }
