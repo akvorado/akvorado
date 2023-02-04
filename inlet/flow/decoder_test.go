24e00d8fb7e7e20dad489fef19358230857f24a5
Vincent Bernat <vincent@bernat.ch>
2023-02-04 14:25:55 +0100

    bench: initial

--- inlet/flow/decoder_test.go
+++ inlet/flow/decoder_test.go
@@ -8,13 +8,13 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/gogo/protobuf/proto"
-
 	"akvorado/common/helpers"
 	"akvorado/common/reporter"
 	"akvorado/inlet/flow/decoder"
 	"akvorado/inlet/flow/decoder/netflow"
 	"akvorado/inlet/flow/decoder/sflow"
+
+	"github.com/golang/protobuf/proto"
 )
 
 // The goal is to benchmark flow decoding + encoding to protobuf
@@ -26,42 +26,60 @@ func BenchmarkDecodeEncodeNetflow(b *testing.B) {
 	template := helpers.ReadPcapPayload(b, filepath.Join("decoder", "netflow", "testdata", "options-template-257.pcap"))
 	got := nfdecoder.Decode(decoder.RawFlow{Payload: template, Source: net.ParseIP("127.0.0.1")})
 	if got == nil || len(got) != 0 {
-		b.Fatalf("Decode() error on options template")
+		b.Fatal("Decode() error on options template")
 	}
 	data := helpers.ReadPcapPayload(b, filepath.Join("decoder", "netflow", "testdata", "options-data-257.pcap"))
 	got = nfdecoder.Decode(decoder.RawFlow{Payload: data, Source: net.ParseIP("127.0.0.1")})
 	if got == nil || len(got) != 0 {
-		b.Fatalf("Decode() error on options data")
+		b.Fatal("Decode() error on options data")
 	}
 	template = helpers.ReadPcapPayload(b, filepath.Join("decoder", "netflow", "testdata", "template-260.pcap"))
 	got = nfdecoder.Decode(decoder.RawFlow{Payload: template, Source: net.ParseIP("127.0.0.1")})
 	if got == nil || len(got) != 0 {
-		b.Fatalf("Decode() error on template")
+		b.Fatal("Decode() error on template")
 	}
 	data = helpers.ReadPcapPayload(b, filepath.Join("decoder", "netflow", "testdata", "data-260.pcap"))
-	for i := 0; i < b.N; i++ {
-		got = nfdecoder.Decode(decoder.RawFlow{Payload: data, Source: net.ParseIP("127.0.0.1")})
-		for _, flow := range got {
-			buf := proto.NewBuffer([]byte{})
-			if err := buf.EncodeMessage(flow); err != nil {
-				b.Fatalf("EncodeMessage() error:\n%+v", err)
-			}
+
+	for _, withEncoding := range []bool{true, false} {
+		title := "with encoding"
+		if !withEncoding {
+			title = "without encoding"
 		}
+		b.Run(title, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				got = nfdecoder.Decode(decoder.RawFlow{Payload: data, Source: net.ParseIP("127.0.0.1")})
+				if withEncoding {
+					for _, flow := range got {
+						buf := proto.NewBuffer([]byte{})
+						buf.EncodeMessage(flow)
+					}
+				}
+			}
+		})
 	}
 }
 
 func BenchmarkDecodeEncodeSflow(b *testing.B) {
 	r := reporter.NewMock(b)
 	sdecoder := sflow.New(r)
-
 	data := helpers.ReadPcapPayload(b, filepath.Join("decoder", "sflow", "testdata", "data-1140.pcap"))
-	for i := 0; i < b.N; i++ {
-		got := sdecoder.Decode(decoder.RawFlow{Payload: data, Source: net.ParseIP("127.0.0.1")})
-		for _, flow := range got {
-			buf := proto.NewBuffer([]byte{})
-			if err := buf.EncodeMessage(flow); err != nil {
-				b.Fatalf("EncodeMessage() error:\n%+v", err)
-			}
+
+	for _, withEncoding := range []bool{true, false} {
+		title := "with encoding"
+		if !withEncoding {
+			title = "without encoding"
 		}
+		var got []*decoder.FlowMessage
+		b.Run(title, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				got = sdecoder.Decode(decoder.RawFlow{Payload: data, Source: net.ParseIP("127.0.0.1")})
+				if withEncoding {
+					for _, flow := range got {
+						buf := proto.NewBuffer([]byte{})
+						buf.EncodeMessage(flow)
+					}
+				}
+			}
+		})
 	}
 }
