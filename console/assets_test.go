a0cc8377c02a949d61f098beb537efc6ac0ba029
Vincent Bernat <vincent@bernat.ch>
2023-07-16 23:26:09 +0200

    console: fix path to SVG asset

--- console/assets_test.go
+++ console/assets_test.go
@@ -33,7 +33,7 @@ func TestServeAssets(t *testing.T) {
 					ContentType: "text/html; charset=utf-8",
 					FirstLines:  []string{"<!doctype html>"},
 				}, {
-					URL:         "/assets/akvorado.399701ee.svg",
+					URL:         "/assets/akvorado-399701ee.svg",
 					ContentType: "image/svg+xml",
 					FirstLines:  []string{`<?xml version="1.0" encoding="UTF-8" standalone="no"?>`},
 				}, {
