bab71588eaca52476385136af1b3d5da0b927291
Vincent Bernat <vincent@bernat.ch>
2023-07-30 19:57:04 +0200

    cmd: also test a full component start

--- cmd/components_test.go
+++ cmd/components_test.go
@@ -6,6 +6,7 @@ package cmd_test
 import (
 	"errors"
 	"testing"
+	"time"
 
 	"akvorado/cmd"
 	"akvorado/common/daemon"
@@ -30,17 +31,17 @@ func (c *Stopable) Stop() error {
 	return nil
 }
 
-type ComponentStartStop struct {
-	Startable
-	Stopable
-}
-type ComponentStop struct {
-	Stopable
-}
-type ComponentStart struct {
-	Startable
-}
 type (
+	ComponentStartStop struct {
+		Startable
+		Stopable
+	}
+	ComponentStop struct {
+		Stopable
+	}
+	ComponentStart struct {
+		Startable
+	}
 	ComponentNone       struct{}
 	ComponentStartError struct {
 		Stopable
@@ -51,7 +52,7 @@ func (c ComponentStartError) Start() error {
 	return errors.New("nooo")
 }
 
-func TestStartStop(t *testing.T) {
+func TestStartStopError(t *testing.T) {
 	r := reporter.NewMock(t)
 	daemonComponent := daemon.NewMock(t)
 	otherComponents := []interface{}{
@@ -80,6 +81,41 @@ func TestStartStop(t *testing.T) {
 		&ComponentNone{},
 		&ComponentStartError{},
 		&ComponentStartStop{},
+	}
+	if diff := helpers.Diff(otherComponents, expected); diff != "" {
+		t.Errorf("StartStopComponents() (-got, +want):\n%s", diff)
+	}
+}
+
+func TestStartStop(t *testing.T) {
+	r := reporter.NewMock(t)
+	daemonComponent := daemon.NewMock(t)
+	otherComponents := []interface{}{
+		&ComponentStartStop{},
+		&ComponentStop{},
+		&ComponentStart{},
+		&ComponentNone{},
+	}
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		daemonComponent.Terminate()
+	}()
+	if err := cmd.StartStopComponents(r, daemonComponent, otherComponents); err != nil {
+		t.Errorf("StartStopComponents() error:\n%+v", err)
+	}
+
+	expected := []interface{}{
+		&ComponentStartStop{
+			Startable: Startable{Started: true},
+			Stopable:  Stopable{Stopped: true},
+		},
+		&ComponentStop{
+			Stopable: Stopable{Stopped: true},
+		},
+		&ComponentStart{
+			Startable: Startable{Started: true},
+		},
+		&ComponentNone{},
 	}
 	if diff := helpers.Diff(otherComponents, expected); diff != "" {
 		t.Errorf("StartStopComponents() (-got, +want):\n%s", diff)
