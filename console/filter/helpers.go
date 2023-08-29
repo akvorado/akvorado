2f8fdc479afee14e7160701ad29cf3a14449ccdf
Vincent Bernat <vincent@bernat.ch>
2023-08-29 16:53:58 +0200

    console/filter: improve parser by returning nested slice of elements
    Instead of returning directly the SQL string, we return a nested slice
    of elements, some of them being columns. They can be reverted easily and
    we can know if they require the main table without needing to encode
    some marker into the string. This is more robust this way.

--- console/filter/helpers.go
+++ console/filter/helpers.go
@@ -5,6 +5,7 @@
 package filter
 
 import (
+	"errors"
 	"fmt"
 	"net/netip"
 	"strings"
@@ -22,69 +23,119 @@ type Meta struct {
 	MainTableRequired bool
 }
 
-const mainTableOnlyMarker = "@@M!M...🫠@@"
-
-// extractMeta will extract metadata from parsed expression.
-func (c *current) extractMeta(expr string, meta *Meta) string {
-	newExpr := strings.Replace(expr, mainTableOnlyMarker, "", -1)
-	if len(newExpr) != len(expr) {
-		meta.MainTableRequired = true
+// flattenExpr takes an expression and flattens it to a slice of strings. It
+// also handles metadata for columns.
+func (c *current) flattenExpr(expr []any, meta *Meta) []string {
+	// Helpers for columns: reverse direction and extract metadata.
+	reverseColumn := func(col schema.Column) schema.Column {
+		name := col.Name
+		if meta.ReverseDirection {
+			var candidate string
+			sch := c.globalStore["meta"].(*Meta).Schema
+			if strings.HasPrefix(name, "Src") {
+				candidate = "Dst" + name[3:]
+			}
+			if strings.HasPrefix(name, "Dst") {
+				candidate = "Src" + name[3:]
+			}
+			if strings.HasPrefix(name, "In") {
+				candidate = "Out" + name[2:]
+			}
+			if strings.HasPrefix(name, "Out") {
+				candidate = "In" + name[3:]
+			}
+			if column, ok := sch.LookupColumnByName(candidate); ok {
+				return *column
+			}
+		}
+		return col
 	}
-	return newExpr
+	metaColumn := func(col schema.Column) schema.Column {
+		col = reverseColumn(col)
+		if col.ClickHouseMainOnly {
+			meta.MainTableRequired = true
+		}
+		return col
+	}
+	var result []string // flattened, pre-join
+	for i := range expr {
+		switch s := expr[i].(type) {
+		case schema.Column:
+			s = metaColumn(s)
+			result = append(result, s.Name)
+		case string:
+			result = append(result, s)
+		case []any:
+			result = append(result, c.flattenExpr(s, meta)...)
+		case uint8, uint16, uint32, uint64, uint, int8, int16, int32, int64, int:
+			result = append(result, fmt.Sprintf("%d", s))
+		default:
+			result = append(result, fmt.Sprintf("%s", s))
+		}
+	}
+	return result
 }
 
-// encodeMeta encode metadata inside column name.
-func encodeMeta(column schema.Column) string {
-	if !column.ClickHouseMainOnly {
-		return column.Name
+// compileExpr compile an expression to a SQL string. An expression is a nested
+// slices of elements. Some elements are schema.Column and are reverted if
+// needed and metadata-related information are extracted. Elements are flattened
+// and joined with spaces (with a few exceptions).
+func (c *current) compileExpr(expr []any, meta *Meta) string {
+	var b strings.Builder
+	sl := c.flattenExpr(expr, meta)
+	for i := range sl {
+		// Do we add a space between sl[i-1] and sl[i]?
+		if i == 0 {
+			b.WriteString(sl[0])
+			continue
+		}
+		last := sl[i-1][len(sl[i-1])-1:]
+		first := sl[i][:1]
+		if first != "," && first != ")" && first != " " && last != "(" && last != " " {
+			b.WriteString(" ")
+		}
+		b.WriteString(sl[i])
 	}
-	return fmt.Sprintf("%s%s", column.Name, mainTableOnlyMarker)
+	return b.String()
 }
 
 // acceptColumn normalizes and returns the matched column name. It should be
-// used in predicate code blocks. It uses a special marker if the column
-// requires the main table. This is quite hacky and it should be ensured that
-// the column name is preserved during parsing (notably for rules we know the
-// column name, we should still use the output of this function).
-func (c *current) acceptColumn() (string, error) {
+// used in predicate code blocks.
+func (c *current) acceptColumn() (schema.Column, error) {
 	name := string(c.text)
-	schema := c.globalStore["meta"].(*Meta).Schema
-	for _, column := range schema.Columns() {
+	sch := c.globalStore["meta"].(*Meta).Schema
+	for _, column := range sch.Columns() {
 		if strings.EqualFold(name, column.Name) {
-			if c.globalStore["meta"].(*Meta).ReverseDirection {
-				var candidate string
-				if strings.HasPrefix(name, "Src") {
-					candidate = "Dst" + name[3:]
-				}
-				if strings.HasPrefix(name, "Dst") {
-					candidate = "Src" + name[3:]
-				}
-				if strings.HasPrefix(name, "In") {
-					candidate = "Out" + name[2:]
-				}
-				if strings.HasPrefix(name, "Out") {
-					candidate = "In" + name[3:]
-				}
-				if column, ok := schema.LookupColumnByName(candidate); ok {
-					return encodeMeta(*column), nil
-				}
-			}
-			return encodeMeta(column), nil
+			return column, nil
 		}
 	}
-	return "", fmt.Errorf("unknown column %q", name)
+	return schema.Column{}, fmt.Errorf("unknown column %q", name)
 }
 
-// metaColumn remembers the matched column name in meta data. It should be used
-// in state change blocks. Unfortunately, it cannot extract matched text, so it
-// should be provided.
-func (c *current) metaColumn(name string) error {
-	if column, ok := c.globalStore["meta"].(*Meta).Schema.LookupColumnByName(name); ok {
-		if column.ClickHouseMainOnly {
-			c.state["main-table-only"] = true
-		}
+// getColumn gets a column by its name.
+func (c *current) getColumn(name string) schema.Column {
+	sch := c.globalStore["meta"].(*Meta).Schema
+	if column, ok := sch.LookupColumnByName(name); ok {
+		return *column
 	}
-	return nil
+	return schema.Column{}
+}
+
+// parsePrefix parses a source or destination prefix to SQL.
+func (c *current) parsePrefix(direction string) ([]any, error) {
+	net, err := netip.ParsePrefix(string(c.text))
+	if err != nil {
+		return []any{}, errors.New("expecting a prefix")
+	}
+	prefix := "::ffff:"
+	if net.Addr().Is6() {
+		prefix = ""
+	}
+	return []any{
+		fmt.Sprintf("BETWEEN toIPv6('%s%s') AND toIPv6('%s%s') AND",
+			prefix, net.Masked().Addr().String(), prefix, lastIP(net).String()),
+		c.getColumn(fmt.Sprintf("%sNetMask", direction)), "=", net.Bits(),
+	}, nil
 }
 
 func lastIP(subnet netip.Prefix) netip.Addr {
