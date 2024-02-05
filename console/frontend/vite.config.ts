ab0bd1774852d1e4f420a2d06722c81644046043
Vincent Bernat <vincent@bernat.ch>
2024-02-05 22:34:00 +0100

    console/frontend: don't do a full coverage for JS
    Only compute coverage when there are tests. I don't have the resources
    to do a full coverage for JS.

--- console/frontend/vite.config.ts
+++ console/frontend/vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
     coverage: {
       reporter: ["text-summary", "html", "cobertura"],
       reportsDirectory: "../../test/js",
+      all: false,
     },
   },
   server: {
