0413e4f467c215579915733981692d3466afacd3
Vincent Bernat <vincent@bernat.ch>
2024-06-03 07:29:12 +0200

    docker: optimize a bit download of ipinfo database

--- docker/ipinfo-geoipupdate.sh
+++ docker/ipinfo-geoipupdate.sh
@@ -5,15 +5,26 @@ trap exit TERM
 while true; do
     for DATABASE in ${IPINFO_DATABASES}; do
         RESPONSE=$(curl \
-            -s -w '%{http_code}' -L -o "${DATABASE}.mmdb.new" \
+            --silent \
+            --write-out '%{http_code}' \
+            --remote-time \
+            --location \
+            --output "${DATABASE}.mmdb.new" \
+            --time-cond "${DATABASE}.mmdb" \
             "https://ipinfo.io/data/free/${DATABASE}.mmdb?token=${IPINFO_TOKEN}")
-        if [ "$RESPONSE" != "200" ]; then
-            echo "$RESPONSE Failed to download ${DATABASE}.mmdb database."
-            rm "${DATABASE}.mmdb.new" 2> /dev/null
-        else
-            echo "${DATABASE}.mmdb database downloaded in /data volume."
-            mv "${DATABASE}.mmdb.new" "${DATABASE}.mmdb"
-        fi
+        case "$RESPONSE" in
+            200)
+                echo "${DATABASE}.mmdb database downloaded in /data volume."
+                mv "${DATABASE}.mmdb.new" "${DATABASE}.mmdb"
+                ;;
+            304)
+                echo "${DATABASE}.mmdb database not modified."
+                ;;
+            *)
+                echo "Failed to download ${DATABASE}.mmdb database (HTTP error $RESPONSE)."
+                rm "${DATABASE}.mmdb.new" 2> /dev/null
+                ;;
+        esac
     done
 
     sleep "$UPDATE_FREQUENCY" &
