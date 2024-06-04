09621f651f78541171e4ce87b00b9d35a48174eb
Vincent Bernat <vincent@bernat.ch>
2024-06-04 19:04:22 +0200

    docker: use ipinfo checksums to decide if new version available

--- docker/ipinfo-geoipupdate.sh
+++ docker/ipinfo-geoipupdate.sh
@@ -3,25 +3,28 @@
 trap exit TERM
 
 while true; do
-    UPDATE_FREQUENCY_MINUTES="$(qalc -t -s 'max decimals 0' "${UPDATE_FREQUENCY} to minutes" \
-        | cut -f1 -d' ')"
     for DATABASE in ${IPINFO_DATABASES}; do
-        find "${DATABASE}.mmdb" -mmin +${UPDATE_FREQUENCY_MINUTES} -print | grep -q . || continue
+        if [ -f ${DATABASE}.mmdb ]; then
+            # Is it up-to-date?
+            LOCAL=$(sha256sum ${DATABASE}.mmdb | awk '{print $1}')
+            REMOTE=$(curl --silent https://ipinfo.io/data/free/${DATABASE}.mmdb/checksums?token=${IPINFO_TOKEN} \
+                | sed -n 's/.*"sha256": *"\([a-f0-9]*\)".*/\1/p')
+            if [ "$LOCAL" = "$REMOTE" ]; then
+                echo "${DATABASE}.mmdb is up-to-date"
+                continue
+            fi
+        fi
         RESPONSE=$(curl \
             --silent \
             --write-out '%{http_code}' \
             --remote-time \
             --location \
             --output "${DATABASE}.mmdb.new" \
-            --time-cond "${DATABASE}.mmdb" \
             "https://ipinfo.io/data/free/${DATABASE}.mmdb?token=${IPINFO_TOKEN}")
         case "$RESPONSE" in
             200)
                 echo "${DATABASE}.mmdb database downloaded in /data volume."
                 mv "${DATABASE}.mmdb.new" "${DATABASE}.mmdb"
-                ;;
-            304)
-                echo "${DATABASE}.mmdb database not modified."
                 ;;
             *)
                 echo "Failed to download ${DATABASE}.mmdb database (HTTP error $RESPONSE)."
