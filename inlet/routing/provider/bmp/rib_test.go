03748f66f4ddc8a61e7078e6090c006fe9a6543a
Gerhard Bogner <gbogner@anexia-it.com>
2025-03-19 10:17:10 +0100

    add check for plen in TestRTAEqual

--- inlet/routing/provider/bmp/rib_test.go
+++ inlet/routing/provider/bmp/rib_test.go
@@ -166,6 +166,18 @@ func TestRTAEqual(t *testing.T) {
 			routeAttributes{asn: 2038, largeCommunities: []bgp.LargeCommunity{{ASN: 1, LocalData1: 2, LocalData2: 3}, {ASN: 3, LocalData1: 4, LocalData2: 5}, {ASN: 5, LocalData1: 6, LocalData2: 7}}},
 			false,
 		},
+		{
+			helpers.Mark(),
+			routeAttributes{plen: 48},
+			routeAttributes{plen: 48},
+			true,
+		},
+		{
+			helpers.Mark(),
+			routeAttributes{plen: 48},
+			routeAttributes{plen: 49},
+			false,
+		},
 	}
 outer:
 	for try := 3; try >= 0; try-- {
