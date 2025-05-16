233865cfe08f845120c6cc76ec8a8eebb7f51b78
Vincent Bernat <vincent@bernat.ch>
2025-05-16 20:27:09 +0200

    inlet/flow: fix description of Start for file input

--- inlet/flow/input/file/root.go
+++ inlet/flow/input/file/root.go
@@ -44,7 +44,7 @@ func (configuration *Configuration) New(r *reporter.Reporter, daemon daemon.Comp
 	return input, nil
 }
 
-// Start starts listening to the provided UDP socket and producing flows.
+// Start starts reading the provided PCAP files to produce flows in a loop.
 func (in *Input) Start() (<-chan []*schema.FlowMessage, error) {
 	in.r.Info().Msg("file input starting")
 	in.t.Go(func() error {
